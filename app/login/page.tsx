e8289060c4f0d581e2aa1d95575a4d4a76d43c46
Xenren <kamren@infinitejest.net>
2024-04-02 12:38:55 -0700

    fixed todo page; moved/named submit button for proper component structure

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 import { headers } from "next/headers";
 import { createClient } from "@/lib/supabase/server";
 import { redirect } from "next/navigation";
-import { SubmitButton } from "./submit-button";
+import { SubmitButton } from "@/components/ui/SubmitButton";
 
 export default function Login({
   searchParams,
