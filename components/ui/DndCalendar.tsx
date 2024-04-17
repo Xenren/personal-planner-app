ad7ff2ef1fc2a542616485f520d0ceed53ae095c
Xenren <kamren@infinitejest.net>
2024-04-17 11:24:05 -0700

    fixed calendar bugs

--- components/ui/DndCalendar.tsx
+++ components/ui/DndCalendar.tsx
@@ -1,15 +1,23 @@
 "use client";
 
-import React, { useState, useMemo, useCallback, useEffect } from 'react';
-import { Calendar, View, momentLocalizer, stringOrDate } from 'react-big-calendar';
-import moment from 'moment';
-import withDragAndDrop from 'react-big-calendar/lib/addons/dragAndDrop';
-import 'react-big-calendar/lib/addons/dragAndDrop/styles.css';
-import 'react-big-calendar/lib/css/react-big-calendar.css';
-import useCalendarEvents from '@/lib/supabase/useCalendarEvents';
-import { getUsername } from '@/lib/supabase/client';
-import EventModal from './EventModal';
-import { CalendarEvent, UpdateCalendarEventParams } from '@/lib/supabase/useCalendarEvents'
+import React, { useState, useMemo, useCallback, useEffect } from "react";
+import {
+  Calendar,
+  View,
+  momentLocalizer,
+  stringOrDate,
+} from "react-big-calendar";
+import moment from "moment";
+import withDragAndDrop from "react-big-calendar/lib/addons/dragAndDrop";
+import "react-big-calendar/lib/addons/dragAndDrop/styles.css";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+import useCalendarEvents from "@/lib/supabase/useCalendarEvents";
+import { getUsername } from "@/lib/supabase/client";
+import EventModal from "./EventModal";
+import {
+  CalendarEvent,
+  UpdateCalendarEventParams,
+} from "@/lib/supabase/useCalendarEvents";
 
 const localizer = momentLocalizer(moment);
 const DnDCalendar = withDragAndDrop(Calendar);
@@ -30,25 +38,41 @@ const DndCalendar = () => {
     getUserID();
   }, []);
 
-  const { events, addEvent, updateEvent, deleteEvent, loading } = useCalendarEvents(userId);
+  const { events, addEvent, updateEvent, deleteEvent, loading } =
+    useCalendarEvents(userId);
 
-  const onEventResize = (data: { event: any; start: stringOrDate; end: stringOrDate; }) => {
-    console.log(data.event)
+  const onEventResize = (data: {
+    event: any;
+    start: stringOrDate;
+    end: stringOrDate;
+  }) => {
+    console.log(data.event);
     updateEvent(data);
   };
 
-  const onEventDrop = (data: { event: any; start: stringOrDate; end: stringOrDate; }) => {
-    console.log(data.event)
+  const onEventDrop = (data: {
+    event: any;
+    start: stringOrDate;
+    end: stringOrDate;
+  }) => {
+    console.log(data.event);
     updateEvent(data);
   };
 
-  const handleUpdateEvent = async (event: CalendarEvent, title: string, description: string) => {
-    const updateParams: UpdateCalendarEventParams = { event: event, title: title, description: description }
-    console.log(event, title, description)
+  const handleUpdateEvent = async (
+    event: CalendarEvent,
+    title: string,
+    description: string,
+  ) => {
+    const updateParams: UpdateCalendarEventParams = {
+      event: event,
+      title: title,
+      description: description,
+    };
+    console.log(event, title, description);
     await updateEvent(updateParams);
     setEventToEdit(null);
   };
-
 
   // add a test event
   const handleAddEvent = () => {
@@ -56,7 +80,8 @@ const DndCalendar = () => {
       title: "New Event",
       description: "test event creation",
       start: moment().toDate().toISOString(),
-      end: moment().add(1, 'hours').toDate().toISOString(),
+      end: moment().add(1, "hours").toDate().toISOString(),
+      all_day: false,
     };
     addEvent(newEvent);
   };
@@ -64,13 +89,13 @@ const DndCalendar = () => {
   // Function to call when an event is clicked
   const handleEventClick = (event: any) => {
     setEventToEdit(event);
-    console.log(eventToEdit)
+    console.log(eventToEdit);
     setIsModalOpen(true);
   };
 
   // Function to call when confirming deletion
-  const handleConfirmDelete = async (event: { event_id: string; } | null) => {
-    console.log(event)
+  const handleConfirmDelete = async (event: { event_id: string } | null) => {
+    console.log(event);
     await deleteEvent(event?.event_id ?? null);
 
     setIsModalOpen(false);
@@ -78,18 +103,27 @@ const DndCalendar = () => {
   };
 
   // State to control the current view
-  const [currentView, setView] = useState<View>('month');
+  const [currentView, setView] = useState<View>("month");
 
   // Handle view change
-  const onView = useCallback((newView: View) => setView(newView), [setView])
+  const onView = useCallback((newView: View) => setView(newView), [setView]);
 
-  const { defaultDate } = useMemo(() => ({
-    defaultDate: moment().toDate()
-  }), [])
+  const { defaultDate } = useMemo(
+    () => ({
+      defaultDate: moment().toDate(),
+    }),
+    [],
+  );
 
   return (
-    <div className="h-full">
-      <button onClick={handleAddEvent}>Add Event</button> {/* Button to add a test event */}
+    <div className="h-full w-max border-black">
+      <button
+        className="px-4 py-2 rounded-md border border-black bg-neutral-100 text-neutral-700 text-sm hover:shadow-[4px_4px_0px_0px_rgba(0,0,0)] transition duration-200"
+        onClick={handleAddEvent}
+      >
+        Add Event
+      </button>{" "}
+      {/* Button to add a test event */}
       {loading ? (
         <p>Loading...</p>
       ) : (
